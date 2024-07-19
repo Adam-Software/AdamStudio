0c772b1c7a7f744a80d2b9430daeaa5d01f0a726
Alexey Nesterov <vertigra.developer@gmail.com>
2024-07-19 11:33:25 +1000

    Worked wersion with thump

--- AdamStudio/Views/MainWindow.xaml.cs
+++ AdamStudio/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using MahApps.Metro.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace AdamStudio.Views
 {
@@ -7,6 +9,23 @@ namespace AdamStudio.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Thumb_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            Thumb  t = sender as Thumb;
+            t.Cursor = Cursors.Hand;
+        }
+
+        private void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            PopupWindow.Height = PopupWindow.Height - e.VerticalChange;   
+        }
+
+        private void Thumb_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            Thumb t = sender as Thumb;
+            t.Cursor = null;
         }
     }
 }
